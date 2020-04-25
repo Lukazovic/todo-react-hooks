2f826ca406ce6384c2a5c630f6cd8b92371e9f29
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-04-25 18:43:16 -0300

    Adding first list of sample todos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import "./App.css";
+import TodoApp from "./TodoApp";
+// import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>TODO</h1>
+      <TodoApp />
     </div>
   );
 }
